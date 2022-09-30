ab6cf70eb63cd520525adf0b9f874fb07b6cb3db
LEE DONG YEONG <kinzinzz@naver.com>
2022-09-30 13:07:50 +0900

    20220930 이동영

---
+++ pair/movie/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+app_name = 'movie'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('create/', views.create, name='create'),
+    path('write/', views.write, name='write'),
+    path('detail/<int:review_pk>', views.detail, name='detail'),
+    path('edit/<int:review_pk>', views.edit, name='edit'),
+    path('upadte/<int:review_pk>', views.update, name='update'),
+    path('delete/<int:review_pk>', views.delete, name='delete'),
+]
--- pair/movie/views.py
+++ pair/movie/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Review
 
 # Create your views here.
+
+def index(request):
+    reviews = Review.objects.all()
+
+    context = {
+        'reviews': reviews
+    }
+    return render(request, 'movie/index.html', context)
+
+def create(request):
+
+    # title = request.GET.get('title')
+    # content = request.GET.get('content')
+    # Review.objects.create(title=title, content=content)
+
+    return render(request, 'movie/create.html')
+
+def write(request):
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+    Review.objects.create(title=title, content=content)
+
+    return redirect('movie:index')
+
+def detail(request, review_pk):
+
+    review = Review.objects.get(id=review_pk)
+
+    context = {
+        'review': review
+    }
+    
+    return render(request, 'movie/detail.html', context)
+
+def edit(request, review_pk):
+    review = Review.objects.get(id=review_pk)
+
+    context = {
+        'review': review,
+    }
+
+    return render (request, 'movie/edit.html',context)
+
+def update(request, review_pk):
+    
+    review = Review.objects.get(id=review_pk)
+    title = request.GET.get('title')
+    content = request.GET.get('content')
+
+    review.title = title
+    review.content = content
+    review.save()
+   
+    return redirect('movie:index')
+
+def delete(request, review_pk):
+    review = Review.objects.get(id=review_pk)
+    review.delete()
+
+    return redirect('movie:index')
